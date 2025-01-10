def4a619f311e52b80b9303ddee56cc82146d0f4
INBoned <bonedjhon93@gmail.com>
2025-01-09 19:14:41 -0500

    atributos de productos

--- app/Http/Controllers/Configuration/LaboratorioController.php
+++ app/Http/Controllers/Configuration/LaboratorioController.php
@@ -32,6 +32,9 @@ class LaboratorioController extends Controller
                     "margen_minimo" => $d->margen_minimo,
                     "color" => $d->color,
                     "created_at" => $d->created_at->format("Y-m-d h:i A"),
+                    "idproveedor" => $d->proveedores->map(function ($p) {
+                        return $p->id;
+                    }),
                     "proveedor_laboratorio" => $d->proveedores->map(function ($p) {
                         return [
                             "id" => $p->id,
@@ -104,6 +107,9 @@ class LaboratorioController extends Controller
                 "margen_minimo" => $laboratorio->margen_minimo,
                 "color" => $laboratorio->color,
                 "created_at" => $laboratorio->created_at->format("Y-m-d h:i A"),
+                "idproveedor" => $laboratorio->proveedores->map(function ($p) {
+                        return $p->id;
+                    }),
                 "proveedor_laboratorio" => $laboratorio->proveedores->map(function ($p) {
                     return [
                         "id" => $p->id,
@@ -127,7 +133,69 @@ class LaboratorioController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $proveedores = json_decode($request->input('proveedores'), true);
+        $request->merge(['proveedores' => $proveedores]);
+        $request->validate([
+            'name' => 'required|string|max:255|unique:laboratorio,name,' . $id,
+            'image_laboratorio' => 'nullable|image|mimes:jpg,jpeg,png|max:2048',
+            'margen_minimo' => 'required|numeric|regex:/^\d+(\.\d{1,2})?$/',
+            'color' => 'required|string|max:7',
+            'proveedores' => 'required|array|min:1',  // Validamos que proveedores sea un array y contenga al menos un proveedor
+            'proveedores.*' => 'exists:proveedor,id', // Verifica que cada ID de proveedor sea válido y exista en la tabla `proveedor`
+        ]);
+
+        $LABORATORIO_EXIST = Laboratorio::withTrashed()
+                            ->where('name',$request->name)
+                            ->where('id','<>',$id)
+                            ->first();
+        if($LABORATORIO_EXIST){
+            if ($LABORATORIO_EXIST->deleted_at) {
+                // Si el departamento está eliminado lógicamente, puedes restaurarlo o actualizarlo
+                return response() -> json([
+                    "message" => 409,
+                    "message_text" => "el laboratorio ".$LABORATORIO_EXIST->name." ya existe pero se encuentra eliminado, ¿Deseas restaurarlo?",
+                    "distrito" => $LABORATORIO_EXIST->id
+                ]);
+            }
+            return response() -> json([
+                "message" => 403,
+                "message_text" => "el laboratorio ".$LABORATORIO_EXIST->name." ya existe"
+            ]);
+        }
+
+        if($request->hasFile("image_laboratorio")){
+            $path = Storage::putFile("laboratorios",$request->file("image_laboratorio"));
+            $request->request->add(["image" => $path]);
+        }
+
+        $laboratorio = Laboratorio::findOrFail($id);
+        $laboratorio->update($request->all());
+
+        $laboratorio->proveedores()->sync($proveedores);
+
+        $laboratorio->load('proveedores');
+
+        return response()->json([
+            "message" => 200,
+            "laboratorio" => [
+                "id" => $laboratorio->id,
+                "name" => $laboratorio->name,
+                "state" => $laboratorio->state ?? 1,
+                "image" => $laboratorio->image ? env("APP_URL")."storage/".$laboratorio->image : '',
+                "margen_minimo" => $laboratorio->margen_minimo,
+                "color" => $laboratorio->color,
+                "created_at" => $laboratorio->created_at->format("Y-m-d h:i A"),
+                "idproveedor" => $laboratorio->proveedores->map(function ($p) {
+                        return $p->id;
+                    }),
+                "proveedor_laboratorio" => $laboratorio->proveedores->map(function ($p) {
+                    return [
+                        "id" => $p->id,
+                        "name" => $p->name,
+                    ];
+                }),
+            ],
+        ]);
     }
 
     /**
@@ -135,6 +203,14 @@ class LaboratorioController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $d = Laboratorio::findOrFail($id);
+        if($d->image){
+            Storage::delete($d->image);
+        }
+        $d->delete();
+
+        return response()->json([
+            "message" => 200
+        ]);
     }
 }
