fc841c2627a7f16aea814d27890c452217323983
INBoned <bonedjhon93@gmail.com>
2025-02-14 17:01:04 -0500

    ajustes

--- app/Http/Controllers/ProductoAtributtes/ProductoEscalaController.php
+++ app/Http/Controllers/ProductoAtributtes/ProductoEscalaController.php
@@ -38,22 +38,72 @@ class ProductoEscalaController extends Controller
 
         $producto = Producto::findOrFail($id);
 
-        $existeEscala = $producto->get_escalas()->where('cantidad',$request->cantidad)->first();
-
-        if($existeEscala){
+        if($request->precio >= $producto->pventa){
             return response()->json([
                 "message" => 403,
-                "message_text" => "ya existe una escala con esa cantidad"
+                "message_text" => "el precio de la escala no puede ser mayor al precio normal del producto (S/ ".$producto->pventa.")"
             ], 422);
         }
 
-        $existePrecio = $producto->get_escalas()->where('precio',$request->precio)->first();
+        $escalas = $producto->get_escalas()->orderBy('cantidad','desc')->get();
+        if($escalas->isNotEmpty()){
+            $existeEscala = $escalas->where('cantidad', $request->cantidad)->first();
+            if ($existeEscala) {
+                return response()->json([
+                    "message" => 403,
+                    "message_text" => "ya existe una escala con esa cantidad"
+                ], 422);
+            }
 
-        if($existePrecio){
-            return response()->json([
-                "message" => 403,
-                "message_text" => "ya existe una escala con ese precio"
-            ], 422);
+            $existePrecio = $escalas->where('precio', $request->precio)->first();
+            if ($existePrecio) {
+                return response()->json([
+                    "message" => 403,
+                    "message_text" => "ya existe una escala con ese precio"
+                ], 422);
+            }
+
+            $escalaAnterior = null;
+            $escalaSiguiente = null;
+
+            foreach ($escalas as $index => $escala) {
+                if ($escala->cantidad < $request->cantidad) {
+                    $escalaSiguiente = $escala;
+                    $escalaAnterior = $escalas[$index - 1] ?? null;
+                    break;
+                }
+            }
+
+            if (!isset($escalaSiguiente)) {
+                $escalaAnterior = $escalas[count($escalas) - 1]; // Asignamos la última escala como escalaAnterior
+                $escalaSiguiente = null;
+            }
+
+            if ($escalaAnterior && $escalaSiguiente) {
+                // La nueva escala está entre la escala anterior y la siguiente
+                if ($request->precio > $escalaSiguiente->precio || $request->precio < $escalaAnterior->precio) {
+                    return response()->json([
+                        "message" => 403,
+                        "message_text" => "se encontro una incoherencia, el <span class='text-success'>precio</span> de la escala (".$request->cantidad." a S/".$request->precio.") debe ser menor que la escala de ".$escalaSiguiente->cantidad." a S/ ".$escalaSiguiente->precio." y mayor que la escala de ".$escalaAnterior->cantidad." a S/ ".$escalaAnterior->precio
+                    ], 422);
+                }
+            } elseif ($escalaAnterior) {
+                // La nueva escala es menor que todas las escalas existentes
+                if ($request->precio < $escalaAnterior->precio) {
+                    return response()->json([
+                        "message" => 403,
+                        "message_text" => "se encontro una incoherencia, el <span class='text-success'>precio</span> de la escala (".$request->cantidad." a S/".$request->precio.") debe ser mayor que la escala de ".$escalaAnterior->cantidad." a S/ ".$escalaAnterior->precio." y menor que el precio normal del producto S/".$producto->pventa
+                    ], 422);
+                }
+            } elseif ($escalaSiguiente) {
+                // La nueva escala es mayor que todas las escalas existentes
+                if ($request->precio > $escalaSiguiente->precio) {
+                    return response()->json([
+                        "message" => 403,
+                        "message_text" => "se encontro una incoherencia, el <span class='text-success'>precio</span> de la escala (".$request->cantidad." a S/".$request->precio.") debe ser menor que la escala de ".$escalaSiguiente->cantidad." a S/ ".$escalaSiguiente->precio 
+                    ], 422);
+                }
+            }
         }
 
         // Crear una nueva escala asociada al producto
@@ -70,7 +120,6 @@ class ProductoEscalaController extends Controller
                 "precio" => $escala->precio,
                 "state" => $escala->state ?? 1,
             ],
-
             "escalas_activas" => $producto->get_escalas()->where('state', 1)->count(),
             "escalas_inactivas" => $producto->get_escalas()->where('state', 0)->count(),
         ]);
@@ -84,6 +133,13 @@ class ProductoEscalaController extends Controller
         ]);
 
         $producto = Producto::findOrFail($id);
+
+        if($request->precio >= $producto->pventa){
+            return response()->json([
+                "message" => 403,
+                "message_text" => "el precio de la escala no puede ser mayor al precio normal del producto (S/ ".$producto->pventa.")"
+            ], 422);
+        }
 
         $existeEscala = $producto->get_escalas()->where('cantidad',$request->cantidad)
                                             ->where('id','<>', $escalaId)
@@ -107,7 +163,70 @@ class ProductoEscalaController extends Controller
             ], 422);
         }
 
-        $escala = $producto->get_escalas()->findOrFail($escalaId);  // Buscar la escala
+        $escalas = $producto->get_escalas()->orderBy('cantidad','desc')->get();
+        
+        $escala = $producto->get_escalas()->findOrFail($escalaId);
+
+        if($escala->cantidad == $request->cantidad && $escala->precio == $request->precio){
+            return response()->json([
+                "escala" => [
+                    "id" => $escala->id,
+                    "cantidad" => $escala->cantidad,
+                    "precio" => $escala->precio,
+                    "state" => $escala->state ?? 1,
+                ],
+    
+                "escalas_activas" => $producto->get_escalas()->where('state', 1)->count(),
+                "escalas_inactivas" => $producto->get_escalas()->where('state', 0)->count(),
+            ]);
+        }
+
+        $escalas = $escalas->reject(function($item) use ($escalaId) {
+            return $item->id == $escalaId;
+        });
+
+        $escalaAnterior = null;
+        $escalaSiguiente = null;
+
+        foreach ($escalas as $index => $esc) {
+            if ($esc->cantidad < $request->cantidad) {
+                $escalaSiguiente = $esc;
+                $escalaAnterior = $esc[$index - 1] ?? null;
+                break;
+            }
+        }
+
+        if (!isset($escalaSiguiente)) {
+            $escalaAnterior = $escalas[count($escalas) - 1]; // Asignamos la última escala como escalaAnterior
+            $escalaSiguiente = null;
+        }
+
+        if ($escalaAnterior && $escalaSiguiente) {
+            // La nueva escala está entre la escala anterior y la siguiente
+            if ($request->precio > $escalaSiguiente->precio || $request->precio < $escalaAnterior->precio) {
+                return response()->json([
+                    "message" => 403,
+                    "message_text" => "se encontro una incoherencia, el <span class='text-success'>precio</span> de la escala (".$request->cantidad." a S/".$request->precio.") debe ser menor que la escala de ".$escalaSiguiente->cantidad." a S/ ".$escalaSiguiente->precio." y mayor que la escala de ".$escalaAnterior->cantidad." a S/ ".$escalaAnterior->precio
+                ], 422);
+            }
+        } elseif ($escalaAnterior) {
+            // La nueva escala es menor que todas las escalas existentes
+            if ($request->precio < $escalaAnterior->precio) {
+                return response()->json([
+                    "message" => 403,
+                    "message_text" => "se encontro una incoherencia, el <span class='text-success'>precio</span> de la escala (".$request->cantidad." a S/".$request->precio.") debe ser mayor que la escala de ".$escalaAnterior->cantidad." a S/ ".$escalaAnterior->precio." y menor que el precio normal del producto S/".$producto->pventa
+                ], 422);
+            }
+        } elseif ($escalaSiguiente) {
+            // La nueva escala es mayor que todas las escalas existentes
+            if ($request->precio > $escalaSiguiente->precio) {
+                return response()->json([
+                    "message" => 403,
+                    "message_text" => "se encontro una incoherencia, el <span class='text-success'>precio</span> de la escala (".$request->cantidad." a S/".$request->precio.") debe ser menor que la escala de ".$escalaSiguiente->cantidad." a S/ ".$escalaSiguiente->precio 
+                ], 422);
+            }
+        }
+
         $escala->delete();
 
         // Actualizar los datos de la escala
@@ -142,6 +261,69 @@ class ProductoEscalaController extends Controller
             'message' => 'Escala eliminada con éxito',
             "escalas_activas" => $producto->get_escalas()->where('state', 1)->count(),
             "escalas_inactivas" => $producto->get_escalas()->where('state', 0)->count(),
+        ]);
+    }
+
+    public function updateState(Request $request, string $id, $escalaId)
+    {   
+        $request->validate([
+            'state' => 'required|boolean',
+        ]);
+        $producto = Producto::findOrFail($id);
+        $escala = $producto->get_escalas()->findOrFail($escalaId);  // Buscar la escala
+
+        // Eliminar la escala
+        $escala->update([
+            'state' => $request->state
+        ]);
+
+        return response()->json([
+            "escala" => [
+                "id" => $escala->id,
+                "cantidad" => $escala->cantidad,
+                "precio" => $escala->precio,
+                "state" => $escala->state,
+            ],
+
+            "escalas_activas" => $producto->get_escalas()->where('state', 1)->count(),
+            "escalas_inactivas" => $producto->get_escalas()->where('state', 0)->count(),
+        ]);
+    }
+
+    public function updateAllEscalasState(Request $request, string $id)
+    {
+        // Validación del estado
+        $request->validate([
+            'state' => 'required|boolean',
+        ]);
+
+        // Obtener el producto
+        $producto = Producto::findOrFail($id);
+
+        // Actualizar el estado de todas las escalas del producto
+        $producto->get_escalas()->each(function ($escala) use ($request) {
+            $escala->update([
+                'state' => $request->state,
+            ]);
+        });
+
+        // Contar las escalas activas e inactivas después de la actualización
+        $escalas_activas = $producto->get_escalas()->where('state', 1)->count();
+        $escalas_inactivas = $producto->get_escalas()->where('state', 0)->count();
+
+        $escalas = $producto->get_escalas()->orderBy('cantidad','asc')->get();
+
+        return response()->json([
+            "escalas" => $escalas->map(function($b){
+                    return [
+                        "id" => $b->id,
+                        "cantidad" => $b->cantidad,
+                        "precio" => $b->precio,
+                        "state" => $b->state,
+                    ];
+                }),
+            "escalas_activas" => $escalas_activas,
+            "escalas_inactivas" => $escalas_inactivas,
         ]);
     }
 }
--- routes/routes/producto_atributtes/escalasProducto.php
+++ routes/routes/producto_atributtes/escalasProducto.php
@@ -11,6 +11,8 @@ Route::group([
 ], function () {
     Route::get('atributtes/productos/config_escalas/{id}', [ProductoEscalaController::class, 'index']);
     Route::put('atributtes/productos/config_escalas/{productoId}/{escalaId}', [ProductoEscalaController::class, 'update']);
+    Route::put('atributtes/productos/config_escalas/state_escala/{productoId}/{escalaId}', [ProductoEscalaController::class, 'updateState']);
+    Route::put('atributtes/productos/config_escalas/general/state/all_escalas/{id}', [ProductoEscalaController::class ,'updateAllEscalasState']);
     Route::delete('atributtes/productos/config_escalas/{productoId}/{escalaId}', [ProductoEscalaController::class, 'delete']);
     Route::post('atributtes/productos/config_escalas/{id}', [ProductoEscalaController::class, 'store']);
 });
